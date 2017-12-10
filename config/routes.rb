6a8685e8cbf280714cad4506fb20a962ab1e6e8c
“Alice <aliceletourneur@icloud.com>
2017-12-10 18:34:00 +0000

    Add sign up feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'users#new'
+  # These routes will be for signup. The first renders a form in the browser, the second will
+  # receive the form and create a user in our database using the data given to us by the user.
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 end
